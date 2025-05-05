68651d3cae30179cd6bb5cc1042c5becc9abeefe
Aman Roy <royaman56456@gmail.com>
2025-05-05 10:35:59 +0530

    Day-21 Code (5/5/25)

---
+++ Day-21/minimum_rotated_sorted_array.js
@@ -0,0 +1,22 @@
+function findMin(nums){
+   let left = 0;
+   let right = nums.length-1;
+   let ans = nums[0];
+
+   while(left <= right){
+    let mid = Math.floor(left + (right - left) / 2);
+    if(nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1]){
+        return nums[mid];
+    }else if(nums[mid] >= nums[0]){
+        left = mid + 1;
+    }else{
+        ans = nums[mid];
+        end = mid - 1;
+    }
+   }
+   return ans;
+}
+console.log(findMin([3,4,5,1,2])); // 1
+
+// TC = O(logn)
+// SC = O(1)
