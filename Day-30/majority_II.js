7a9928a09677fa1f4a71dfe48490b8b78ecdc941
Aman Roy <royaman56456@gmail.com>
2025-05-14 11:27:36 +0530

    Day-30 Code (14/5/25)

--- Day-30/majority_II.js
+++ Day-30/majority_II.js
@@ -1,29 +1,78 @@
 // Brute Force
 
-function findMajority(arr) {
-  // Your code goes here
-  let n = arr.length;
-  let votes = Math.floor(n / 3);
-  let freqMap = {};
-  let result = [];
+// function findMajority(arr) {
+//   // Your code goes here
+//   let n = arr.length;
+//   let votes = Math.floor(n / 3);
+//   let freqMap = {};
+//   let result = [];
 
-  for (let i = 0; i < n; i++) {
-    let num = arr[i];
-    if (freqMap[num] === undefined) {
-      freqMap[num] = 1;
-    } else {
-      freqMap[num]++;
-    }
-  }
+//   for (let i = 0; i < n; i++) {
+//     let num = arr[i];
+//     if (freqMap[num] === undefined) {
+//       freqMap[num] = 1;
+//     } else {
+//       freqMap[num]++;
+//     }
+//   }
 
-  for (let num in freqMap) {
-    if (freqMap[num] > votes) {
-      result.push(Number(num));
-    }
-  }
-  return result;
-}
-console.log(findMajority([2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]));
+//   for (let num in freqMap) {
+//     if (freqMap[num] > votes) {
+//       result.push(Number(num));
+//     }
+//   }
+//   return result;
+// }
+// console.log(findMajority([2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6])); // [ 5, 6 ]
 
 // TC = O(n)
 // SC = O(n)
+
+
+// Using Boyer Moore
+
+function majorityII(arr){
+   let n = arr.length;
+   let candidate1 = null, candidate2 = null;
+   let count1 = 0, count2 = 0;
+
+   for(let i=0;i<n;i++){
+      if(arr[i] === candidate1){
+        count1++;
+      }else if(arr[i] === candidate2){
+        count2++;
+      }else if(count1 === 0){
+        candidate1 = arr[i];
+        count1 = 1;
+      }else if(count2 === 0){
+        candidate2 = arr[i];
+        count2 = 1;
+       }else{
+        count1--;
+        count2--;
+       }
+   }
+   
+   count1 = 0;
+   count2 = 0;
+
+   for(let num of arr){
+    if(num === candidate1){
+        count1++;
+    }else if(num === candidate2){
+        count2++;
+    }
+   }
+    let result = [];
+    if(count1 > n/3) result.push(candidate1);
+    if(count2 > n/3) result.push(candidate2);
+
+    return result;
+
+}
+console.log(majorityII([2,1,5,5,5,5,6,6,6,6,6])); // [5,6]
+
+// TC = O(n)
+// SC = O(1)
+
+
---
+++ Day-30/stock_buy_and_sell_II.js
@@ -0,0 +1,17 @@
+ function maximumProfit(prices) {
+        // code here
+        if(prices === null || prices.length <= 1){
+            return 0;
+        }
+        
+        let totalProfit = 0;
+        for(let i=1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                totalProfit += prices[i] - prices[i-1];
+            }
+        }
+        return totalProfit;
+        
+    }
+    console.log(maximumProfit([100, 180, 260, 310, 40, 535, 695]));
+    
