9188cf90f9771312e5c3b05ca60368b36a90aca3
Aman Roy <royaman56456@gmail.com>
2025-05-20 18:52:33 +0530

    Day-36 Code (20/5/25)

--- Day-35/contiguous_array.js
+++ Day-35/contiguous_array.js
@@ -1,23 +1,29 @@
 // Brute Force
 
-function findMaxLength(nums) {
-  let maxLen = 0;
+// function findMaxLength(nums) {
+//   let maxLen = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    let count0 = 0,
-      count1 = 0;
-    for (let j = i; j < nums.length; j++) {
-      if (count0 === 0) {
-        count0++;
-      } else {
-        count1++;
-      }
+//   for (let i = 0; i < nums.length; i++) {
+//     let count0 = 0,
+//       count1 = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       if (count0 === 0) {
+//         count0++;
+//       } else {
+//         count1++;
+//       }
 
-      if (count0 === count1) {
-        maxLen = Math.max(maxLen, j - i + 1);
-      }
-    }
-  }
-  return maxLen;
-}
-console.log(findMaxLength([0, 1, 0]));
+//       if (count0 === count1) {
+//         maxLen = Math.max(maxLen, j - i + 1);
+//       }
+//     }
+//   }
+//   return maxLen;
+// }
+// console.log(findMaxLength([0, 1, 0])); // 2
+
+// TC = O(n^2)
+// SC = O(1)
+
+
+// Optimize
---
+++ Day-36/subarray_sum_equals_k.js
@@ -0,0 +1,46 @@
+// Brute Force
+
+// function subarraySum(nums, k){
+//     let count = 0;
+//     let n = nums.length;
+
+//     for(let i=0;i<n;i++){
+//         let sum = 0;
+//         for(let j=i;j<n;j++){
+//             sum += nums[j];
+
+//             if(sum === k){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+// console.log(subarraySum([1,1,1], 2)); // 2
+
+// TC = O(n^2)
+// SC = O(1)
+
+
+// Optimize
+
+function subarraySum(nums, k){
+     let count = 0;
+     let sum = 0;
+
+     let prefixSum = new Map();
+     
+     prefixSum.set(0, 1);
+
+     for(let i=0;i<nums.length;i++){
+        sum += nums[i];
+
+        if(prefixSum.has(sum - k)){
+            count += prefixSum.get(sum - k);
+        }
+
+        prefixSum.set(sum, (prefixSum.get(sum) || 0) + 1);
+     }
+     return count;
+}
+console.log(subarraySum([1,1,1], 2));
