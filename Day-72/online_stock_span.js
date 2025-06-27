9df1e42b7bf9e182cb91e3106d81d9e6ecde0f0b
Aman Roy <royaman56456@gmail.com>
2025-06-27 10:04:53 +0530

    Deleted build folder to reduce repo size

---
+++ Day-72/online_stock_span.js
@@ -0,0 +1,28 @@
+function StockSpanner() {
+    this.stack = [];   // stack of indices
+    this.prices = [];  // store all prices
+}
+
+StockSpanner.prototype.next = function(price) {
+    let i = this.prices.length; // index of current price
+    this.prices.push(price);    // store price
+
+    // pop all smaller or equal prices
+    while (this.stack.length > 0 && this.prices[this.stack[this.stack.length - 1]] <= price) {
+        this.stack.pop();
+    }
+
+    // calculate span
+    let span;
+    if (this.stack.length == 0) {
+        span = i + 1;
+    } else {
+        span = i - this.stack[this.stack.length - 1];
+    }
+
+    this.stack.push(i); // push current index
+    return span;
+};
+
+// TC = O(n)
+// SC = O(n)
